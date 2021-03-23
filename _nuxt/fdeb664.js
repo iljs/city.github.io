3f5b66bfbf2a531ce0fabe6aa97274700333a062
iljs <38344415+iljs@users.noreply.github.com>
2021-03-23 15:15:06 +0300

    Add files via upload

---
+++ _nuxt/fdeb664.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{259:function(e,n,t){"use strict";t.r(n);var c={layout:"index"},l=t(8),component=Object(l.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"7c6d14e2",null);n.default=component.exports}}]);
